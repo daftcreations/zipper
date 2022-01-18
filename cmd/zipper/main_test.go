aadb3620a15621e3d1aac41933424bb3a4e9404e
Pratik Balar <pratikbalar123@gmail.com>
2022-01-19 02:58:22 +0530

    test: tests in loop

--- cmd/zipper/main_test.go
+++ cmd/zipper/main_test.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"runtime"
 	"testing"
+	"time"
 
 	. "github.com/mholt/archiver/v3"
 	"github.com/stretchr/testify/assert"
@@ -13,90 +15,93 @@ import (
 )
 
 func TestE2E(t *testing.T) {
-	// ch := make(chan struct{}, runtime.NumCPU())
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	noOfTmpFiles := 20
+	// ch := make(chan struct{}, runtime.NumCPU())
+	for _, noOfTmpFile := range []int{0, 1, 2, 3, 4, 5, 10, 25, 50} {
+		// noOfTmpFiles := 20
 
-	// Create temp dir for creating test files
-	pwd, err := os.Getwd()
-	if err != nil {
-		t.Error("Error getting working dir:", err)
-	}
-	testDir := filepath.Join(pwd, "testdir")
-	tmpFilesPath := filepath.Join(testDir, "testfiles")
-	if err = os.MkdirAll(tmpFilesPath, 0777); err != nil {
-		t.Error("Error creating ", tmpFilesPath, " dir:", err)
-	}
-
-	// Create testfiles
-	for i := 0; i < noOfTmpFiles; i++ {
-		// tmpFileName := filepath.Join(tmpFilesPath, fmt.Sprint(i)+"-tmpfile.txt")
-		// ch <- struct{}{}
-		func(tmpFileName string) {
-			testFile, err := os.Create(tmpFileName)
-			if err != nil {
-				t.Error("Error while creating file at ", tmpFileName, " :", err)
-			}
-			testFile.Write([]byte(loremipsum.New().Sentences(10000)))
-			testFile.Close()
-			// <-ch
-		}(filepath.Join(tmpFilesPath, fmt.Sprint(i)+"-tmpfile.txt"))
-	}
-
-	// Create zip Test
-	if err = crateZips(tmpFilesPath, 3000000); err != nil {
-		t.Error("Error creating zip from path", tmpFilesPath, ": ", err)
-	}
-
-	// Remove tmp files, not zips
-	defer func() {
-		if err = os.RemoveAll(testDir); err != nil {
-			t.Error("Error removing ", tmpFilesPath, ":", err)
+		// Create temp dir for creating test files
+		pwd, err := os.Getwd()
+		if err != nil {
+			t.Error("Error getting working dir:", err)
 		}
-	}()
-
-	// Extract files
-	extractedZips := filepath.Join(pwd, "extractedzips")
-	if err = os.MkdirAll(extractedZips, 0777); err != nil {
-		t.Error("Error creating ", extractedZips, " dir:", err)
-	}
-	if err = filepath.Walk(pwd,
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if filepath.Ext(info.Name()) == ".zip" {
-				if err = Unarchive(filepath.Join(pwd, info.Name()), extractedZips); err != nil {
-					t.Error("Error while Unarchiving", info.Name(), ":", err)
-				}
-				if err = os.Remove(filepath.Join(pwd, info.Name())); err != nil {
-					t.Error("Error while removing ", filepath.Join(pwd, info.Name()), ":", err)
-				}
-			}
-			return nil
-		}); err != nil {
-		t.Error("Error while walking through", pwd, ":", err)
-	}
-
-	// Count should be equal to no of files created
-	count := 0
-	if err = filepath.Walk(extractedZips,
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if filepath.Ext(info.Name()) == ".txt" {
-				count++
-			}
-			return nil
-		}); err != nil {
-		t.Error("Error while walking through", extractedZips, ":", err)
-	}
-	// Remove extracted files
-	defer func() {
-		if err = os.RemoveAll(extractedZips); err != nil {
-			t.Error("Error removing ", extractedZips, ":", err)
+		testDir := filepath.Join(pwd, "testdir")
+		tmpFilesPath := filepath.Join(testDir, "testfiles")
+		if err = os.MkdirAll(tmpFilesPath, 0777); err != nil {
+			t.Error("Error creating ", tmpFilesPath, " dir:", err)
 		}
-	}()
-	assert.Equal(t, count, noOfTmpFiles, "Extracted files are ", fmt.Sprint(count), " and No of temp files are", noOfTmpFiles)
+
+		// Create testfiles
+		for i := 0; i < noOfTmpFile; i++ {
+			// tmpFileName := filepath.Join(tmpFilesPath, fmt.Sprint(i)+"-tmpfile.txt")
+			// ch <- struct{}{}
+			func(tmpFileName string) {
+				testFile, err := os.Create(tmpFileName)
+				if err != nil {
+					t.Error("Error while creating file at ", tmpFileName, " :", err)
+				}
+				testFile.Write([]byte(loremipsum.New().Sentences(rand.New(rand.NewSource(time.Now().UnixNano())).Intn(10000))))
+				testFile.Close()
+				// <-ch
+			}(filepath.Join(tmpFilesPath, fmt.Sprint(i)+"-tmpfile.txt"))
+		}
+
+		// Create zip Test
+		if err = crateZips(tmpFilesPath, 3000000); err != nil {
+			t.Error("Error creating zip from path", tmpFilesPath, ": ", err)
+		}
+
+		// Remove tmp files, not zips
+		defer func() {
+			if err = os.RemoveAll(testDir); err != nil {
+				t.Error("Error removing ", tmpFilesPath, ":", err)
+			}
+		}()
+
+		// Extract files
+		extractedZips := filepath.Join(pwd, "extractedzips")
+		if err = os.MkdirAll(extractedZips, 0777); err != nil {
+			t.Error("Error creating ", extractedZips, " dir:", err)
+		}
+		if err = filepath.Walk(pwd,
+			func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				if filepath.Ext(info.Name()) == ".zip" {
+					if err = Unarchive(filepath.Join(pwd, info.Name()), extractedZips); err != nil {
+						t.Error("Error while Unarchiving", info.Name(), ":", err)
+					}
+					if err = os.Remove(filepath.Join(pwd, info.Name())); err != nil {
+						t.Error("Error while removing ", filepath.Join(pwd, info.Name()), ":", err)
+					}
+				}
+				return nil
+			}); err != nil {
+			t.Error("Error while walking through", pwd, ":", err)
+		}
+
+		// Count should be equal to no of files created
+		count := 0
+		if err = filepath.Walk(extractedZips,
+			func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				if filepath.Ext(info.Name()) == ".txt" {
+					count++
+				}
+				return nil
+			}); err != nil {
+			t.Error("Error while walking through", extractedZips, ":", err)
+		}
+		// Remove extracted files
+		defer func() {
+			if err = os.RemoveAll(extractedZips); err != nil {
+				t.Error("Error removing ", extractedZips, ":", err)
+			}
+		}()
+		assert.Equal(t, count, noOfTmpFile, "Extracted files are ", fmt.Sprint(count), " and No of temp files are", noOfTmpFile)
+		time.Sleep(time.Second * 2)
+	}
 }
--- go.mod
+++ go.mod
@@ -20,7 +20,6 @@ require (
 
 require (
 	github.com/disintegration/imaging v1.6.2
-	github.com/google/gofuzz v1.2.0
 	github.com/mholt/archiver/v3 v3.5.1
 	github.com/stretchr/testify v1.7.0
 	gopkg.in/loremipsum.v1 v1.1.0

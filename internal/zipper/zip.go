ba743fcc29e3fa19942a9c43b39d71ef64f89e29
Pratik <68642400+pratikbin@users.noreply.github.com>
2022-05-14 13:27:24 +0530

    fix: handle empty dir, add comments, bump go ver

--- internal/zipper/zip.go
+++ internal/zipper/zip.go
@@ -4,6 +4,7 @@ import (
 	"archive/zip"
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -50,14 +51,24 @@ func init() {
 	}
 }
 
+// Create zip size of zipSplitSize bytes from give dirPath Directory
 func CrateZips(dirPath string, zipSplitSize int) error {
-	now := time.Now()
+	// Check if dirPath is empty
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		panic(err)
+	}
+	if len(files) == 0 {
+		log.Fatal("Directory is empty")
+	}
 
+	now := time.Now()
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	dirPath = strings.TrimRight(dirPath, osPathSuffix)
 	zipQueue := make(chan zipTask, runtime.NumCPU()*4)
 	noOfWorker := runtime.NumCPU()
 
+	// Spin up #noOfWorker workers
 	wg.Add(noOfWorker)
 	for i := 0; i < noOfWorker; i++ {
 		go makeArchive(zipQueue)
@@ -70,15 +81,19 @@ func CrateZips(dirPath string, zipSplitSize int) error {
 			if err != nil {
 				return err
 			}
+
+			// Ignore Directory
 			if info.IsDir() {
 				return nil
 			}
 
+			// If file size is less than zipSplitSize, then throw error
 			if info.Size() > int64(zipSplitSize) {
 				return fmt.Errorf("\"%v\" is \"%vKB\" more then %vKB",
 					info.Name(), info.Size()/1000, zipSplitSize/1000)
 			}
 
+			// Add file in queue to be processed by workers
 			queue.Enqueue(filepath.Join(dirPath, info.Name()))
 
 			return nil
@@ -95,6 +110,7 @@ func CrateZips(dirPath string, zipSplitSize int) error {
 		singleFile := fmt.Sprint(queue.Dequeue())
 		filesList = append(filesList, singleFile)
 
+		// EOF Predict zip size
 		zipFile, err := zipWriter.Create(filepath.Base(singleFile))
 		if err != nil {
 			return err
@@ -111,13 +127,17 @@ func CrateZips(dirPath string, zipSplitSize int) error {
 		if err = zipWriter.Flush(); err != nil {
 			return err
 		}
+		// EOF Predict zip size
 
 		totalBytes += zippedFileSize
 
 		if totalBytes > zipSplitSize || queue.Empty() {
+			// Add task to queue if its not empty
 			if !queue.Empty() {
 				queue.Enqueue(singleFile)
 			}
+
+			// Add last task to queue channel, close the channel and break the loop
 			if queue.Empty() {
 				zipQueue <- zipTask{
 					filesList,
@@ -129,6 +149,7 @@ func CrateZips(dirPath string, zipSplitSize int) error {
 				break
 			}
 
+			// Add task to zip queue channel
 			zipQueue <- zipTask{
 				filesList[:len(filesList)-1],
 				fmt.Sprintf("%s-%v.zip", filepath.Base(dirPath), count),
@@ -141,9 +162,12 @@ func CrateZips(dirPath string, zipSplitSize int) error {
 			count++
 		}
 	}
+	// If channel is
 	if _, ok := <-zipQueue; ok {
 		close(zipQueue)
 	}
+
+	// Wait for all workers to finish
 	wg.Wait()
 	fmt.Println("Fin. Took ", time.Since(now))
 	return nil
@@ -163,6 +187,7 @@ func makeArchive(zipQueue chan zipTask) {
 		for _, v := range zipTask.zipFileList[1:] {
 			fmt.Printf(zipTask.format, "", filepath.Base(v), "")
 		}
+		// Adding gap in first row of table
 		for i := 0; i < zipRowSize+filesRowSize+goidRawSize+2; i++ {
 			fmt.Print("-")
 		}
--- internal/zipper/zip_test.go
+++ internal/zipper/zip_test.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"runtime"
 	"testing"
 	"time"
 
@@ -15,98 +14,111 @@ import (
 	"gopkg.in/loremipsum.v1"
 )
 
+// noOfFiles means argument 1 and arg2 means argument 2, and the expected stands for the 'result we expect'
+type addTest struct {
+	noOfFiles, fileLength int
+}
+
+var addTests = []addTest{
+	// {0, 0},
+	{20, 4000},
+	// {100, 10},
+	// {3, 10},
+}
+
 func TestE2E(t *testing.T) {
-	t.Parallel()
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	// noOfTmpFiles := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(50)
-	noOfTmpFiles := 20
+	for _, test := range addTests {
 
-	// Create temp dir for creating test files
-	pwd, err := os.Getwd()
-	if err != nil {
-		t.Error("Error getting working dir:", err)
-	}
-	testDir := filepath.Join(pwd, "testdir")
-	tmpFilesPath := filepath.Join(testDir, "testfiles")
-	if err = os.MkdirAll(tmpFilesPath, 0o750); err != nil {
-		t.Error("Error creating ", tmpFilesPath, " dir:", err)
-	}
+		// noOfTmpFiles := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(50)
+		noOfTmpFiles := test.noOfFiles
 
-	// Create testfiles
-	for i := 0; i < noOfTmpFiles; i++ {
-		// tmpFileName := filepath.Join(tmpFilesPath, fmt.Sprint(i)+"-tmpfile.txt")
-		func(tmpFileName string) {
-			testFile, err := os.Create(filepath.Clean(tmpFileName))
-			if err != nil {
-				t.Error("Error while creating file at ", tmpFileName, " :", err)
-			}
-			// testFile.Write([]byte(loremipsum.New().Sentences(4000)))
-			// testFile.Write([]byte(loremipsum.New().Sentences(rand.New(rand.NewSource(time.Now().UnixNano())).Intn(4000))))
-			_, err = testFile.Write([]byte(loremipsum.New().Sentences(4000)))
-			if err != nil {
-				t.Error(err)
-			}
-			if err = testFile.Close(); err != nil {
-				t.Error(err)
-			}
-		}(filepath.Join(tmpFilesPath, fmt.Sprint(i)+"-tmpfile.txt"))
-	}
-
-	// Create zip Test
-	if err = CrateZips(tmpFilesPath, 3000000); err != nil {
-		t.Error("Error creating zip from path", tmpFilesPath, ": ", err)
-	}
-
-	// Check goroutine leak test in middle
-	// goleak.VerifyNone(t)
-
-	// Remove tmp files, not zips
-	t.Cleanup(func() {
-		if err = os.RemoveAll(testDir); err != nil {
-			t.Error("Error removing ", tmpFilesPath, ":", err)
+		// Create temp dir for creating test files
+		pwd, err := os.Getwd()
+		if err != nil {
+			t.Error("Error getting working dir:", err)
 		}
-	})
+		testDir := filepath.Join(pwd, "testdir")
+		tmpFilesPath := filepath.Join(testDir, "testfiles")
+		if err = os.MkdirAll(tmpFilesPath, 0o750); err != nil {
+			t.Error("Error creating ", tmpFilesPath, " dir:", err)
+		}
 
-	// Extract files
-	extractedZips := filepath.Join(pwd, "extractedzips")
-	if err = os.MkdirAll(extractedZips, 0o750); err != nil {
-		t.Error("Error creating ", extractedZips, " dir:", err)
-	}
-	if err = filepath.Walk(pwd,
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if filepath.Ext(info.Name()) == ".zip" {
-				if err = Unarchive(filepath.Join(pwd, info.Name()), extractedZips); err != nil {
-					t.Error("Error while Unarchiving", info.Name(), ":", err)
+		// Create testfiles
+		for i := 0; i < noOfTmpFiles; i++ {
+			// tmpFileName := filepath.Join(tmpFilesPath, fmt.Sprint(i)+"-tmpfile.txt")
+			func(tmpFileName string) {
+				testFile, err := os.Create(filepath.Clean(tmpFileName))
+				if err != nil {
+					t.Error("Error while creating file at ", tmpFileName, " :", err)
 				}
-				t.Cleanup(func() {
-					if err = os.Remove(filepath.Join(pwd, info.Name())); err != nil {
-						t.Error("Error while removing ", filepath.Join(pwd, info.Name()), ":", err)
-					}
-				})
-			}
-			return nil
-		}); err != nil {
-		t.Error("Error while walking through", pwd, ":", err)
-	}
-
-	// Count should be equal to no of files created
-	count := 0
-	noOfExtractedFiles, err := ioutil.ReadDir(extractedZips)
-	if err != nil {
-		panic(err)
-	}
-
-	// Remove extracted files
-	assert.Equal(t, len(noOfExtractedFiles), noOfTmpFiles, "Extracted files are ", fmt.Sprint(count), " and No of temp files are", noOfTmpFiles)
-	t.Cleanup(func() {
-		if err = os.RemoveAll(extractedZips); err != nil {
-			t.Error("Error removing ", extractedZips, ":", err)
+				/* #nosec */
+				// _, err = testFile.Write([]byte(loremipsum.New().Sentences(int(rand.Int31n(int32(4000))))))
+				_, err = testFile.Write([]byte(loremipsum.New().Sentences(test.fileLength)))
+				if err != nil {
+					t.Error(err)
+				}
+				if err = testFile.Close(); err != nil {
+					t.Error(err)
+				}
+			}(filepath.Join(tmpFilesPath, fmt.Sprint(i)+"-tmpfile.txt"))
 		}
-	})
-	time.Sleep(time.Second * 2)
+
+		// Create zip Test
+		if err = CrateZips(tmpFilesPath, 3000000); err != nil {
+			t.Error("Error creating zip from path", tmpFilesPath, ": ", err)
+		}
+
+		// Check goroutine leak test in middle
+		// goleak.VerifyNone(t)
+
+		// Remove tmp files, not zips
+		t.Cleanup(func() {
+			if err = os.RemoveAll(testDir); err != nil {
+				t.Error("Error removing ", tmpFilesPath, ":", err)
+			}
+		})
+
+		// Extract files
+		extractedZips := filepath.Join(pwd, "testExtractedZips")
+		if err = os.MkdirAll(extractedZips, 0o750); err != nil {
+			t.Error("Error creating ", extractedZips, " dir:", err)
+		}
+		if err = filepath.Walk(pwd,
+			func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				if filepath.Ext(info.Name()) == ".zip" {
+					if err = Unarchive(filepath.Join(pwd, info.Name()), extractedZips); err != nil {
+						t.Error("Error while Unarchiving", info.Name(), ":", err)
+					}
+					t.Cleanup(func() {
+						if err = os.Remove(filepath.Join(pwd, info.Name())); err != nil {
+							t.Error("Error while removing ", filepath.Join(pwd, info.Name()), ":", err)
+						}
+					})
+				}
+				return nil
+			}); err != nil {
+			t.Error("Error while walking through", pwd, ":", err)
+		}
+
+		// Count should be equal to no of files created
+		count := 0
+		noOfExtractedFiles, err := ioutil.ReadDir(extractedZips)
+		if err != nil {
+			panic(err)
+		}
+
+		// Remove extracted files
+		assert.Equal(t, len(noOfExtractedFiles), noOfTmpFiles, "Extracted files are ", fmt.Sprint(count), " and No of temp files are", noOfTmpFiles)
+		t.Cleanup(func() {
+			if err = os.RemoveAll(extractedZips); err != nil {
+				t.Error("Error removing ", extractedZips, ":", err)
+			}
+		})
+		time.Sleep(time.Second * 2)
+	}
 }
 
 // goleak can also be run at the end of every test package by creating a
